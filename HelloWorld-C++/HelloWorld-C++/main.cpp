0db6770733761c0df403810be74efcec1e9156c2
pochia <yap6433089@gmail.com>
2024-07-31 07:34:01 +0800

    lesson 2

--- HelloWorld-C++/HelloWorld-C++/main.cpp
+++ HelloWorld-C++/HelloWorld-C++/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+	string s;
+	cin >> s;
+	cout << "Hello, " << s << "\n";
+	//std::cout << "Hello World" << std::endl;
 	return 0;
 }
---
+++ simple caculate/simple caculate/simple caculate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a , b;
+	cout << "輸入第一個整數 :";  
+	cin >> a;
+	cout << "輸入第二個整數 :";
+	cin >> b;
+
+	cout << "兩數相加之和為 :" << a + b;
+
+	return 0;
+}
